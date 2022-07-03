7dc199e12f94890911b2cf6b0515e7e3de33d2f2
DAN-PC\Daniel <dkchmielewski@gmail.com>
2022-07-03 15:41:50 -0400

    bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length++);
+    // this.log.push(this.log.length++);
+    this.log.push(new Date);
   }
 }
